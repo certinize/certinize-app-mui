a3f789ea12123646b0deebeeca55b20fbc105c29
Hju Kneyck Flores <hjucode@gmail.com>
2022-11-13 22:24:38 +0800

    Impl. app bar

---
+++ src/containers/Dashboard.js
@@ -0,0 +1,15 @@
+import Navbar from "../components/ResponsiveAppBar";
+
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Navbar />
+            <div>
+                <h1>Dashboard</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard;
