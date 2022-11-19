0b6f13891dde01524b01028e24aee19dac31e6ba
Mindy Kay V. Zaracena <mindykayzara05@gmail.com>
2022-11-20 00:48:00 +0800

    Implement landing, profile, and get verified form

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,15 +1,1 @@
-import Navbar from "../components/ResponsiveAppBar";
 
-
-const Dashboard = () => {
-    return (
-        <div>
-            <Navbar />
-            <div>
-                <h1>Dashboard</h1>
-            </div>
-        </div>
-    )
-}
-
-export default Dashboard;
