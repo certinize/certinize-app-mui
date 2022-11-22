efbbe029f7a67b6015cad0ebc1d8a8d24eba0ff6
Hju Kneyck Flores <hjucode@gmail.com>
2022-11-22 09:57:12 +0800

    Upload isser verification form layout

--- src/containers/IssuerVerification.js
+++ src/containers/IssuerVerification.js
@@ -1,20 +1,45 @@
-import { Box, FormControl, TextField, Typography } from "@mui/material";
+/* eslint-disable no-unused-vars */
+import { LoadingButton } from "@mui/lab";
+import {
+  Box,
+  Divider,
+  FormControl,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Container from "@mui/material/Container";
 import { MuiFileInput } from "mui-file-input";
 import React from "react";
+
+import { verifyUser } from "../api/UserAPI";
 
 const boxStyle = {
   marginTop: 4,
   bgcolor: "#F9F9F9",
   borderRadius: 4,
-  boxShadow: "1px 2px 2px 2px #888888 ",
+  boxShadow:
+    "rgb(60 66 87 / 20%) 0rem 0.4375rem 0.875rem 0rem, rgb(0 0 0 / 50%) 0rem 0.1875rem 0.375rem 0rem",
   width: 600,
   padding: 4,
 };
 
 export default function IssuerVerification() {
+  const [loading, setLoading] = React.useState(false);
+  const [verifyReq, setVerifyReq] = React.useState({
+    pubkey: "",
+    info_link: "",
+    official_website: "",
+    official_email: "",
+    organization_id: "",
+    approved: "",
+  });
   const [orgId, setOrgId] = React.useState(null);
   const handleChange = (e) => setOrgId(e);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    console.log(verifyReq);
+  };
 
   return (
     <Container
@@ -28,12 +53,29 @@ export default function IssuerVerification() {
         <Box
           sx={{
             ...boxStyle,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
+            textAlign: "center",
           }}
         >
-          <Typography variant="h2">Get Verified!</Typography>
+          <Typography variant="h4" color="primary" sx={{ fontWeight: "bold" }}>
+            Get Verified!
+          </Typography>
+          <Divider sx={{ marginTop: 2, marginBottom: 2 }} />
+          <Typography paragraph={true} sx={{ textAlign: "justify" }}>
+            <b>
+              With Certinize, people do not have to verify certificates. They
+              only need to validate the issuing body, your organization.
+            </b>{" "}
+            It means to find out if a wallet address used to issue a certificate
+            belongs to an organization.
+          </Typography>
+          <Typography paragraph={true} sx={{ textAlign: "justify" }}>
+            The verification process is optional. People can still verify an
+            issuing body themselves using other means. You don&apos;t have to
+            provide us with your organization&apos;s information, but it will
+            help people authenticate an issuing body more quickly. If you are
+            familiar with Twitter&apos;s verified badge, this process implements
+            the same concept.
+          </Typography>
         </Box>
         <Box sx={boxStyle}>
           <FormControl sx={{ gap: 2, marginBottom: 4 }}>
@@ -49,6 +91,10 @@ export default function IssuerVerification() {
               id="walletPrivateKey"
               label="Enter private key"
               variant="outlined"
+              onChange={(e) =>
+                setVerifyReq({ ...verifyReq, pubkey: e.target.value })
+              }
+              sx={{ maxWidth: "70%" }}
             />
           </FormControl>
         </Box>
@@ -58,7 +104,9 @@ export default function IssuerVerification() {
               2. Provide a link we can use to verify your organization&apos;s
               identity and credibility.
             </Typography>
-            <Typography>Any of the following will work:</Typography>
+            <Typography>
+              <i>Any of the following will work:</i>
+            </Typography>
             <ul>
               <li>
                 <Typography paragraph={true}>
@@ -104,6 +152,10 @@ export default function IssuerVerification() {
               id="verificationLink"
               label="Enter a link"
               variant="outlined"
+              onChange={(e) =>
+                setVerifyReq({ ...verifyReq, info_link: e.target.value })
+              }
+              sx={{ maxWidth: "70%" }}
             />
           </FormControl>
         </Box>
@@ -120,6 +172,10 @@ export default function IssuerVerification() {
               id="officialWebsite"
               label="Official website"
               variant="outlined"
+              onChange={(e) =>
+                setVerifyReq({ ...verifyReq, official_website: e.target.value })
+              }
+              sx={{ maxWidth: "70%" }}
             />
           </FormControl>
         </Box>
@@ -136,6 +192,10 @@ export default function IssuerVerification() {
               id="officialEmail"
               label="Official email"
               variant="outlined"
+              onChange={(e) =>
+                setVerifyReq({ ...verifyReq, official_email: e.target.value })
+              }
+              sx={{ maxWidth: "70%" }}
             />
           </FormControl>
         </Box>
@@ -148,9 +208,22 @@ export default function IssuerVerification() {
               Provide a photo of a valid official organization-issued
               identification document, such as your school ID or company ID.
             </Typography>
-            <MuiFileInput value={orgId} onChange={handleChange} />
+            <MuiFileInput
+              value={orgId}
+              onChange={handleChange}
+              sx={{ maxWidth: "70%", marginRight: "auto" }}
+              placeholder="Choose a file"
+            />
           </FormControl>
         </Box>
+        <LoadingButton
+          loading={loading}
+          onClick={handleSubmit}
+          variant="contained"
+          sx={{ height: 64, marginTop: 4 }}
+        >
+          Submit
+        </LoadingButton>
       </Box>
     </Container>
   );
